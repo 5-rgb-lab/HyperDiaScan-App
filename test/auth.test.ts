551916ad0cd403e74d8c02a07bf51943006e2b48
ZX3C15 <esurena.15k@gmail.com>
2025-09-29 16:13:19 +0800

    asd

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -42,7 +42,7 @@ describe("Auth Helpers", () => {
     const fakeUser = { uid: "123" };
     (signInWithEmailAndPassword as jest.Mock).mockResolvedValueOnce({ user: fakeUser });
 
-    const user = await signInWithEmail("test@example.com", "password");
+    const user = await signInWithEmail("test@example.com", 1234);
 
     expect(signInWithEmailAndPassword).toHaveBeenCalledWith(auth, "test@example.com", "password");
     expect(user).toEqual(fakeUser);
